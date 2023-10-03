2a8e5ecff499b4ee6ef961a3b10101fd65b0e583
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-03 16:12:51 +0430

    add font-family and custom-colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-blue": "hsl(231, 69%, 60%)",
+        "soft-red": "hsl(0, 94%, 66%)",
+
+        "grayish-blue": "hsl(229, 8%, 60%)",
+        "very-dark-blue": " hsl(229, 31%, 21%)",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
