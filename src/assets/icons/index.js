a95bb6dd1874cabd17dc00f8bbe31629690ef8b3
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-03 18:34:56 +0430

    structure images and icons to be export/ export by ease

---
+++ src/assets/icons/index.js
@@ -0,0 +1,8 @@
+import arrow from "./icon-arrow.svg"
+import close from "./icon-close.svg"
+import error from "./icon-error.svg"
+import facebook from "./icon-facebook.svg"
+import hamburger from "./icon-hamburger.svg"
+import twitter from "./icon-twitter.svg"
+
+export {arrow, close, error, facebook, hamburger, twitter}
---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+import bookmark from "./logo-bookmark.svg";
+import chrome from "./logo-chrome.svg";
+import firefox from "./logo-firefox.svg";
+import opera from "./logo-opera.svg";
+import dots from "./bg-dots.svg";
+import tab1 from "./illustration-features-tab-1.svg";
+import tab2 from "./illustration-features-tab-2.svg";
+import tab3 from "./illustration-features-tab-3.svg";
+import hero from "./illustration-hero.svg";
+
+export { bookmark, chrome, dots, firefox, hero, opera, tab1, tab2, tab3 };
